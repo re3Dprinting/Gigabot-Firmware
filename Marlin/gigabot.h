b8d08b1a5eb53e7dd491d463052461739bb2e2b5
plloppii <noahpan323@gmail.com>
2018-06-25 18:17:00 -0500

    Babystepping enabled, Linear Advance default value change to 0.075

--- Marlin/gigabot.h
+++ Marlin/gigabot.h
@@ -266,7 +266,19 @@
   #define SD_CHECK_AND_RETRY
   #define VIKI2
   #define INDIVIDUAL_AXIS_HOMING_MENU
-
+  
+  #define BABYSTEPPING
+  #if ENABLED(BABYSTEPPING)
+	  //#define BABYSTEP_XY              // Also enable X/Y Babystepping. Not supported on DELTA!
+	  #define BABYSTEP_INVERT_Z false    // Change if Z babysteps should go the other way
+	  #define BABYSTEP_MULTIPLICATOR 2   // Babysteps are very small. Increase for faster motion.
+	  //#define BABYSTEP_ZPROBE_OFFSET   // Enable to combine M851 and Babystepping
+	  //#define DOUBLECLICK_FOR_Z_BABYSTEPPING // Double-click on the Status Screen for Z Babystepping.
+	  #define DOUBLECLICK_MAX_INTERVAL 1250 // Maximum interval between clicks, in milliseconds.
+											// Note: Extra time may be added to mitigate controller latency.
+	  //#define BABYSTEP_ZPROBE_GFX_OVERLAY // Enable graphical overlay on Z-offset editor
+  #endif
+  
   #if ENABLED(SDSUPPORT)
     #define SDCARD_RATHERRECENTFIRST
  //#define SDCARD_SORT_ALPHA
@@ -278,6 +290,10 @@
   #define LCD_INFO_MENU
   #define LCD_TIMEOUT_TO_STATUS 120000
   #define LIN_ADVANCE
+  #if ENABLED(LIN_ADVANCE)
+	  #define LIN_ADVANCE_K 0.075  // Unit: mm compression per 1mm/s extruder speed
+	  //#define LA_DEBUG          // If enabled, this will generate debug information output over USB.
+	#endif
 #endif
 
 #define ULTRA_LCD  //general LCD support, also 16x2
